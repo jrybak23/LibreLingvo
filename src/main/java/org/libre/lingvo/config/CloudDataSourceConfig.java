77b7928cc87ac64364f184342c41f6c87a70481e
igorek2312 <rybakigor1996@gmail.com>
2016-12-27 00:00:44 +0200

    get rid of connection pool on cloud

--- src/main/java/org/libre/lingvo/config/CloudDataSourceConfig.java
+++ src/main/java/org/libre/lingvo/config/CloudDataSourceConfig.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by igorek2312 on 28.11.16.
@@ -18,9 +20,16 @@ public class CloudDataSourceConfig extends AbstractCloudConfig {
     public DataSource dataSource() {
         DataSource dataSource = connectionFactory().dataSource();
         if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
-            org.apache.tomcat.jdbc.pool.DataSource tomcatDatasource =
+            /*org.apache.tomcat.jdbc.pool.DataSource tomcatDatasource =
                     (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
-            tomcatDatasource.setConnectionProperties("useUnicode=yes;characterEncoding=utf8;");
+            tomcatDatasource.setConnectionProperties("useUnicode=yes;characterEncoding=utf8;");*/
+            String url = ((org.apache.tomcat.jdbc.pool.DataSource) dataSource).getUrl();
+            DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource(url);
+            Properties properties = new Properties();
+            properties.put("useUnicode", "yes");
+            properties.put("characterEncoding", "utf8");
+            driverManagerDataSource.setConnectionProperties(properties);
+            dataSource = driverManagerDataSource;
         }
         return dataSource;
     }
